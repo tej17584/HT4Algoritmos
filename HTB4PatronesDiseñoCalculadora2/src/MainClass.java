1dd34d6dbef27df8854bf07cab15dd35dd52d391
José Alejandro Tejada Flores <tej17584@uvg.edu.gt>
2018-02-12 11:05:29 -0600

    Primer Commit

---
+++ HTB4PatronesDiseñoCalculadora2/src/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jose Tejada
+ * @author Andrez Urizar
+ * @version 1.0
+ * Esta es la clase principal
+ */
+public class MainClass {
+    
+}
